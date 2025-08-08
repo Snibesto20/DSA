b877312fd1a45e54ff6caafedaed08bdd89326f9
Snibesto20 <snipisrblx@gmail.com>
2025-08-08 13:14:34 +0300

    DSA - Selection sort

---
+++ algorithms/sort algorithms/selection-sort.ts
@@ -0,0 +1,32 @@
+// Selection sort
+
+// Notes
+// - O(n²) time complexity
+// - simple, yet slow algorithm therefore rarely used
+// - Minimizes swaps
+
+function selectionSort(arr: number[]): number[] {
+    for(let i = 0; i < arr.length; i++) {
+        // Find if a smaller value than i exists
+        let smallest = i
+
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[j] < arr[smallest]) {
+                smallest = j
+            }
+        }
+
+        // If there was a smaller value than i, swap them.
+        if(arr[i] > arr[smallest]) {
+            const t = arr[i]
+            arr[i] = arr[smallest]
+            arr[smallest] = t
+        }
+    }
+
+    // Returning the result
+    return arr
+}
+
+// Testing
+const sortedArr = selectionSort([2,13,5,1,-5,27,14,2]);
